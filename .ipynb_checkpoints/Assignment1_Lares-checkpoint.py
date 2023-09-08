2dd9fdc5ddf0ba3a586ec641829bf228217b6918
UGAOscar <55748924+o-lares@users.noreply.github.com>
2023-09-08 14:43:24 -0400

    Initial commit

---
+++ .ipynb_checkpoints/Assignment1_Lares-checkpoint.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Assignment 1 for ELEE6280 - by Oscar Lares
+
+# Some parts of code adapted from these 2 sources: 
+# https://medium.com/data-breach/introduction-to-orb-oriented-fast-and-rotated-brief-4220e8ec40cf
+# https://github.com/tshanmukh/Facial-KeyPoint-Detection/blob/master/ORB.ipynb
+
+# Submitted 2/26/2023
+
+#import cv2 and os module needed
+import os
+import cv2
+
+#specifying folder path and extension for where images are stored
+folder_path = 'D:/OneDrive - University of Georgia/School/Classes/ELEE6280 - Intro Robotics/Assignment 1/first_200_right'
+extension = '.png'
+
+#load images into an array, use os.listdir to access folder path and read images in loop
+images = []
+for filename in sorted(os.listdir(folder_path)):
+    if filename.endswith(extension):
+        image = cv2.imread(os.path.join(folder_path, filename))
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        images.append(gray)
+
+#create ORB feature detector and find ORB keypoints and descriptors for first image
+orb = cv2.ORB_create()
+kp, desc = orb.detectAndCompute(images[0], None)
+
+#create a Brute Force matcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+#initialize total keypoint and matched keypoints to help keep track of accuracy
+total_kp = 0
+matched_kp = 0
+
+#create videowriter object to save tracking results to video format
+fourcc = cv2.VideoWriter_fourcc(*'H264')
+videowriter = cv2.VideoWriter('video_lares.mp4', fourcc, 10, images[0].shape[::-1])
+
+#create loop for remaining images after getting kp and desc for first one in order to track the features
+for i in range(1, len(images)):
+    
+    #find ORB keypoints and descriptors for image 'i'
+    kp2, desc2 = orb.detectAndCompute(images[i], None)
+    
+    #perform the matching between the ORB descriptors of the current image and the previous image
+    matches = bf.match(desc, desc2)
+
+    #the matches with shorter distance are the ones we want
+    matches = sorted(matches, key = lambda x : x.distance)
+            
+    #draw match and keypoints for video write      
+    img_match = cv2.drawMatches(images[i-1], kp, images[i], kp2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    cv2.imshow('Matches', img_match)
+    cv2.waitKey(2)
+    
+    #update kp and desc for the next iteration in the loop
+    kp, desc = kp2, desc2
+    
+    #update the count on matched and total keypoints for accuracy calculations later on
+    total_kp += len(kp2)
+    matched_kp += len(matches)
+    
+    #write tracking results to the output video
+    videowriter.write(img_match)
+    
+#end the video writer object and destroy all windows
+videowriter.release()
+cv2.destroyAllWindows()
+
+# Calculate and display the matching accuracy
+matching_accuracy = matched_kp / total_kp * 100
+print(f'Total keypoints: {total_kp}')
+print(f'Matched keypoints: {matched_kp}')
+print(f'Matching accuracy: {matching_accuracy:.2f}%')
---
+++ .ipynb_checkpoints/readME_Lares-checkpoint.txt
@@ -0,0 +1,19 @@
+Assignment 1 - Oscar Lares - ELEE6280
+Image Tracking using OpenCV in Python
+
+This project aims to demonstrate how to perform image tracking using python. I utilized OpenCV, a computer vision library, to implement the tracking. Image tracking is the process of analyzing successive frames of an image or video sequence to determine the movement of objects within the scene.
+
+Features Used
+I utilized the ORB (Oriented FAST and Rotated BRIEF) feature detector to extract features from the images. These features are basically key points or landmarks that are distinctive and can be easily tracked from one image to another. I settled on ORB over SIFT, mainly to see how this algorithm worked but also because from what I read, it is a much faster algorithm, and it seemed simple to implement. I would like to use SIFT on future homeworks to see how it works in relation to ORB.
+
+Matching Algorithm
+I used the Brute-Force Matcher, a common feature matching algorithm, to match the features between the images. The algorithm computes the Euclidean distance between the feature descriptors in two images, and returns the best match. I mainly saw it being used in the codes I referenced and figured the implementation was simple enough that I could use it fast to first get familiar with this computer vision task.
+
+Tracking Accuracy
+To measure the accuracy of the tracking, I first calculated the number of correctly matched features between consecutive frames, and divided it by the total number of features detected in the previous frame. This gave me a percentage of how many features were accurately tracked.
+
+Results
+The tracking results are displayed as a video which shows the cross frame tracking effect. A tracking accuracy of around 60% was obtained on our dataset of 200 images. I previously tried using the knn match method using k=2 but got worse accuracy results. Additionally, converting the images to black and white also helped to increase accuracy. In the first implementation the accuracy was around 40%. I think the BW images helped create more distinctive keypoints for the algorithm to detect.
+
+Conclusion
+In this project, it was demonstrated how to perform image tracking using OpenCV in Python. I utilized the ORB feature detector and Brute-Force Matcher to track features in the sequence of 200 images provided and outlined the accuracy as well as some other methods tested to land on the final results.
